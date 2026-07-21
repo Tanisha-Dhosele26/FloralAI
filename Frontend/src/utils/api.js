b751740f02569e26ae633cd81283d3689461afeb
Tanisha Dhosele <tanishadhosele@gmail.com>
2026-07-21 19:11:06 +0530

    Updated the frontend api.js api url for render deployment

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://floralai.onrender.com";
 
 export const authFetch = async (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
